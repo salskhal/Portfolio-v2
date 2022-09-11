7bfe1de6d0bb81ae0651c08aead557a3984419c0
salskhal <olaniyisal@gmail.com>
2022-09-11 11:33:49 +0100

    feat/ simple mockup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen" id="home">
     <Nav />
-    <div class="" >
+    <div class="mt-10" >
       <Landing  />
       <About id="about" />
       <Skills id="skills" />
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="bg-main text-gray-100 py-4 px-6 md:px-16 shadow md:flex justify-between">
+  <div class="bg-main text-gray-100 py-4 px-6 md:px-16 shadow md:flex justify-between w-full fixed">
     <div class="flex items-center cursor-pointer">
       <Logo />
     </div>
     <span class="absolute md:hidden right-6 top-[-15px] cursor-pointer" @click="toggleNav">
       <Hamburger class="w-8"/>
     </span>
-    <ul class="md:flex md:items-center px-10 pb-10 md:px-0 md:pb-0 md:static absolute bg-main  md:w-auto w-full top-14 duration-300 ease-in" :class="[open ? 'left-0' : 'left-[-100%]']">
+    <ul class="md:flex md:items-center px-10 pb-10 md:px-0 md:pb-0 md:static absolute bg-main  md:w-auto w-full top-14 md:shadow-none  shadow duration-500 ease-in" :class="[open ? 'left-0' : 'left-[-100%]']">
       <li class="md:mx-4 md:my-0 my-6" v-for="(link, index) in links" :key="index" @click="closeNav">
-        <a :href="link.link" class="text-xl hover:text-green-500">{{link.name}}</a>
+        <a :href="link.link" class="text-xl hover:text-[hsla(0,0%,100%,.52)]">{{link.name}}</a>
       </li>
     </ul>
   </div>
