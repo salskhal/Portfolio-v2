f5445c5c9edd04fd72dbf71d964e7de528e379fb
salskhal <olaniyisal@gmail.com>
2022-09-13 07:56:33 +0100

    removed aos library

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import { AOS } from 'aos';
 import './index.css'
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
+
 // import './assets/main.css'
 
 createApp(App).mount('#app')
